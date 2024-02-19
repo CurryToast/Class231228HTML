0ea97bc55bc5eccaec173e085ae357c50f25479b
CurryToast <gusqjacjswo39@gmail.com>
2024-02-19 09:14:05 +0900

    회원가입 스타일 수정

--- signup_ex/signup.js
+++ signup_ex/signup.js
@@ -13,14 +13,22 @@ const regexTest = (el, regex) => {
 document.forms[0].name.addEventListener('change', (e) => {
     regexTest(e.target, nameRegex);
 });
-
 document.forms[0].id.addEventListener('change', (e) => {
     regexTest(e.target, idRegex);
 });
-
 document.forms[0].password.addEventListener('change', (e) => {
     regexTest(e.target, pwRegex);
 });
+
+const formatDate = (vdate) => {
+    const year = vdate.getFullYear();
+    const month = (vdate.getMonth() + 1).toString().padStart(2, '0');
+    const date = vdate.getDate().toString().padStart(2, '0');
+
+    return [year, month, date].join('-');
+};
+
+document.forms[0].birthday.max = formatDate(new Date());
 
 const openConfirm = () => {
     return confirm('회원가입 하시겠습니까?');
